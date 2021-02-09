b01abaf7affc7b6d8df9d527f6638eecf38b5bc4
Asaf Ohayon <asaf@sysbind.co.il>
2021-02-10 01:39:34 +0200

    start reading moodle's config.php

--- autobackup.go
+++ autobackup.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Auto Backup Starting..")
+	f, err := os.Open("config.php")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
