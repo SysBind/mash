08d9f94c97000b37afd73514b4ac6b62de4e41d6
chen <chen@sysbind.co.il>
2022-04-26 14:57:38 +0300

    fixed skipping courses that don't have backup and logs

--- moodle/course/eventlog.go
+++ moodle/course/eventlog.go
@@ -9,9 +9,15 @@ import (
 )
 
 func ModifiedSince(db database.Database, cid uint64, since uint64) bool {
+
 	query := fmt.Sprintf("SELECT COUNT(id) FROM mdl_logstore_standard_log WHERE courseid = %d AND timecreated > %d AND NOT crud = 'r' AND NOT target = 'course_backup'", cid, since)
 	var count int
 	_ = db.QueryRow(query).Scan(&count)
+	if count > 0 {
+		return true
+	}
+	query = fmt.Sprintf("SELECT COUNT(id) FROM mdl_logstore_standard_log WHERE courseid = %d AND target = 'course_backup'", cid)
+	_ = db.QueryRow(query).Scan(&count)
+	return count == 0
 
-	return (count > 0)
 }
